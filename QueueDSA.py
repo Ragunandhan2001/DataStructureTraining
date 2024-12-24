99bd1e1722be540d1fb0e287d0e301c47e3b2d51
Ragunandhan2001 <717822z137@kce.ac.in>
2024-12-24 16:06:33 +0530

    queue data stucture

---
+++ QueueDSA.py
@@ -0,0 +1,40 @@
+# class Queue:
+#
+#     def __init__(self):
+#         self.List=[]
+#
+#     def enqueue(self,elements):
+#         self.List.append(elements)
+#
+#     def dequeue(self):
+#         if self.List:
+#             self.List.pop(0)
+#         else:
+#             print("Nothing to delete")
+#
+#     def size(self):
+#         if self.List:
+#             print(len(self.List))
+#         else:
+#             print("nothing to calculate")
+#
+#     def is_empty(self):
+#         if self.List:
+#             print(False)
+#         else:
+#             print(True)
+#
+# queue = Queue()
+# queue.enqueue("12")
+# queue.enqueue(45)
+# queue.enqueue("ragu")
+# queue.enqueue("nandhan")
+# queue.enqueue("90")
+# queue.enqueue("54")
+# queue.size()
+# queue.dequeue()
+# queue.size()
+# queue.List.clear()
+# queue.size()
+# queue.is_empty()
+
